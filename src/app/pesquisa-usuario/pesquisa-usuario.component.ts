8b0576e62ec7bc2ab9359b49cc796dd9531e7e52
emamaia <ema_maia@hotmail.com>
2022-08-11 18:10:49 -0300

    Transforma objeto em array

--- src/app/pesquisa-usuario/pesquisa-usuario.component.ts
+++ src/app/pesquisa-usuario/pesquisa-usuario.component.ts
@@ -16,36 +16,60 @@ export class PesquisaUsuarioComponent implements OnInit {
   usuarios: any;
   listaUsuarios = []
 
-   constructor(
-      private pesquisaUsuarioService: PesquisaUsuarioService, 
-            
+  escondeSection = true;
+
+  listaUsers = []
+  
+  constructor(
+    private pesquisaUsuarioService: PesquisaUsuarioService,
+
   ) {
     console.log(pesquisaUsuarioService);
-   
+
   }
 
   ngOnInit(): void {
     this.listaPaises = this.pesquisaUsuarioService.getListaPaises();
     this.listaGender = this.pesquisaUsuarioService.getListaGender();
     this.pesquisaUsuarioService.getPesquisa()
-    .subscribe(response => {
-     this.usuarios = response
-     this.listaUsuarios = this.usuarios.results
-     console.log(this.listaUsuarios)
-    })
+      .subscribe(response => {
+        this.usuarios = response
+        this.listaUsuarios = this.usuarios.results
+        console.log(this.listaUsuarios)
+      })
   }
-  
-  
+
+
   onSubmit(form) {
     this.valorBusca = form.value;
-    console.log(this.valorBusca);      
-         
+    console.log(this.valorBusca);
+    this.escondeSection = false;
+
+    this.listaUsers = Object.keys(this.valorBusca).map(key =>({
+      type: key,
+      value: this.valorBusca[key]
+    }))
+
+    console.log('USER', this.listaUsers)
+    
   }
 
   onCheckBoxChange(event) {
     console.log(event.target.value)
- 
+   
   }
+
+  // tranformaArray(valorInput){
+  // let filterUser =[]
+  //  for(key of Object.keys(valorInput)){
+  //   filterUser.push(
+  //     [key, valorInput[key]]
+  //   )
+  //  }
+  //  return filterUser
+  // }
+    
   
+
 
 }
